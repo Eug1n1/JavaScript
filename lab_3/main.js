f87cfb5a96ad5e4d06f141e4ae5a32ede4e66d9c
Evgeniy Shumskiy <vkzheny@gmail.com>
2020-03-09 16:05:09 +0300

    lab_3 task 3

--- lab_3/main.js
+++ lab_3/main.js
@@ -1,7 +1,6 @@
-
 function first(){
     NumArray = new Array();
-
+    document.write('<body style="color: red; font-family: Ariel, sans-serif">')
     NumArray[0] = 6 * Math.pow(Math.PI, 2) + 3 * Math.exp(8);
     NumArray[1] = 2 * Math.cos(4) + Math.cos(12) + 8 * Math.exp(3);
     NumArray[2] = 3 * Math.sin(9) + Math.log(5) + Math.sqrt(3);
@@ -31,7 +30,7 @@ function first(){
 }
 
 function two(){
-
+    document.write('<body style="color: green; font-family: Ariel, sans-serif">')
     Arr = new Array('pow', 'pop', 'push', 'shift', 'round', 'floor', 'slice', 'sort')
 
     document.writeln('Arr = ')
@@ -83,5 +82,26 @@ function two(){
 }
 
 function three(){
-    
+    document.write('<body style="color: blue; font-family: Ariel, sans-serif">')
+    str = new String('Shumskiy Evgeniy Sergeevich');
+
+    document.write(str + '<br>');
+
+    document.write(str.toUpperCase() + '<br>');
+    document.write(str.toLowerCase() + '<br>');
+
+    document.write(str.toLowerCase().concat(str.toUpperCase()) + '<br>');
+
+    document.write(str.replace('Shumskiy Evgeniy Sergeevich', 'FIO') + '<br>');
+}
+
+function finale(){
+    document.write('<body style="color: yellow; font-family: Ariel, sans-serif; background-color: black;">')
+    date = new Date();
+    document.write('<table border="1px" style="box-shadow: 10px 10px 50px white"><tr><td>Год</td><td>' + date.getFullYear() + '</td></tr>');
+    document.write('<tr><td>Месяц</td><td>' + date.getMonth() + '</td></tr>');
+    document.write('<tr><td>День</td><td>' + date.getDay() + '</td></tr>');
+    document.write('<tr><td>Час</td><td>' + date.getHours() + '</td></tr>');
+    document.write('<tr><td>Минуты</td><td>' + date.getMinutes() + '</td></tr>');
+    document.write('<tr><td>Секунды</td><td>' + date.getSeconds() + '</td></tr>');
 }
